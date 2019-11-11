c4643bf2fa57a77acfc1f5fecd57975f661629c1
macid <macidori98@gmail.com>
2019-11-11 16:50:30 +0200

    database

--- app/src/main/java/com/example/questionlarry/DetailsDialog.java
+++ app/src/main/java/com/example/questionlarry/DetailsDialog.java
@@ -17,7 +17,8 @@ public class DetailsDialog extends AppCompatDialogFragment {
     private Form form;
     private int position;
 
-    private TextView tv_name, tv_birthdate, tv_location;
+    private TextView tv_name, tv_birthdate, tv_location, tv_gender, tv_hobby, tv_department,
+        tv_yearOfStudy, tv_expactation ;
 
     public DetailsDialog(){}
 
@@ -36,17 +37,40 @@ public class DetailsDialog extends AppCompatDialogFragment {
         tv_birthdate = view.findViewById(R.id.detail_birthdate);
         tv_name = view.findViewById(R.id.detail_name);
         tv_location = view.findViewById(R.id.detail_location);
+        tv_gender = view.findViewById(R.id.detail_gender);
+        tv_hobby = view.findViewById(R.id.detail_hobby);
+        tv_department = view.findViewById(R.id.detail_department);
+        tv_yearOfStudy = view.findViewById(R.id.detail_year);
+        tv_expactation = view.findViewById(R.id.detail_expactation);
 
         tv_location.setText(form.getLocation());
         tv_birthdate.setText(form.getBirthDate());
         tv_name.setText(form.getName());
+
+        tv_gender.setText(form.getGender());
+
+        String hobbylist="";
+        for (int i = 0; i < form.getHobbies().size(); ++i){
+            if (i == form.getHobbies().size()-1){
+                hobbylist = hobbylist + form.getHobbies().get(i) ;
+            } else {
+                hobbylist = hobbylist + form.getHobbies().get(i) + "\n";
+            }
+
+        }
+
+        tv_hobby.setText(hobbylist);
+
+        tv_department.setText(form.getDepartment());
+        tv_yearOfStudy.setText(form.getYearOfStudy());
+        tv_expactation.setText(form.getLongAnswer());
 
         doSomething(builder,view);
         return builder.create();
     }
 
     private void doSomething(AlertDialog.Builder builder, View view){
-        builder.setView(view).setTitle("Dialog").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        builder.setView(view).setTitle("Details").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 
